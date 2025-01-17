88c7ebfb4855868b074a878b414480415ed48d43
EshimovShaxzod <eshimov474@gmail.com>
2025-01-17 14:12:51 +0500

    about section done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import About from "./components/about";
 import Hero from "./components/hero";
 
 const App = () => {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
       <Hero />
+
+      <About />
     </main>
   );
 };
